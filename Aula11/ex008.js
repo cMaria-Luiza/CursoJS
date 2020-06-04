c5a4287b4cea13d60a09d878affe362bff1e3011
Malu <mariacalixto@cc.ci.ufpb.br>
2020-06-04 19:54:49 -0300

    first commit

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.5;
+console.log(`A velociade do seu carro é ${vel}`)
+if(vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. Multado!');
+}
+
+console.log('Dirija sempre usando cinto de segunraça!');
